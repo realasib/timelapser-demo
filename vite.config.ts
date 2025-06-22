216af10c9baa23da615a203f81866b247ab4f0a5
Asib Hossen <dev.asib@proton.me>
2025-06-22 08:38:25 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-    base: '/' // Replace with your GitHub repo name
+    base: '/timelapser-demo/' // Replace with your GitHub repo name
 }));
